68273e1f8b290d0b43fe9ec5c8cfc60e3a871b16
Dhanto Santika <dhantosy@gmail.com>
2012-01-31 19:33:45 +0800

    Custom CSS

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,32 +1,53 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
+    end
+  it "should have the right title" do
+	get 'home'
+	response.should have_selector("title",
+	:content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
-  end
-
-  describe "GET 'about'" do
-    it "returns http success" do
-      get 'about'
-      response.should be_success
+  it "should have the right title" do
+	get 'contact'
+	response.should have_selector("title",
+	:content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 
-  describe "GET 'help'" do
-    it "returns http success" do
+ describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  it "should have the right title" do
+	get 'about'
+	response.should have_selector("title",
+	:content => "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+ describe "GET 'help'" do
+    it "should be successful" do
       get 'help'
       response.should be_success
+    end
+  it "should have the right title" do
+	get 'help'
+	response.should have_selector("title",
+	:content => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
